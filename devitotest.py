49b5e79f6889abc1e21be1c60a733f697c7f0e2b
devitos-yale <sophia.devito@yale.edu>
2021-09-13 22:38:49 -0400

    Update devitotest.py

--- devitotest.py
+++ devitotest.py
@@ -7,11 +7,19 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton,
 class Window2(QMainWindow):                           # <===
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Window22222")
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Resize This Window")
+
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        #self.setWindowTitle("Resize This Window")
 
         self.title = "First Window"
         self.top = 100
@@ -19,31 +27,57 @@ class Window(QMainWindow):
         self.width = 680
         self.height = 500
 
-        self.pushButton = QPushButton("Start", self)
-        self.pushButton.move(275, 200)
+        self.pushButton = QPushButton("START", self)
+        self.pushButton.resize(200,50)
+        self.pushButton.move(220, 300)
         self.pushButton.setToolTip("<h3>Start the Session</h3>")
 
         self.pushButton.clicked.connect(self.window2)              # <===
 
+        #NEW CODE
+        self.lineEntry1 = QLineEdit(self) #Projector number textbox
+        self.lineEntry1.move(285, 150)
+        self.lineEntry1.resize(100,20)
+        number = lineEntry1.text()
+
+        self.lineEntry2 = QLineEdit(self) #Orientation textbox
+        self.lineEntry2.move(285, 200)
+        self.lineEntry2.resize(100,20)
+
+        self.pushButton = QPushButton() 
+        self.pushButton.setObjectName("set") 
+        self.pushButton.resize(100,50)
+        self.pushButton.move(430, 160)
+        self.pushButton.clicked.connect(self.button_clicked)
+
         self.main_window()
 
     def main_window(self):
-        self.label = QLabel("Manager", self)
-        self.label.move(285, 175)
+        self.label = QLabel("Input Projector Number:", self)
+        self.label.resize(300,20)
+        self.label.move(120, 150)
+
+        self.label = QLabel("Input Orientation:", self)
+        self.label.resize(300,20)
+        self.label.move(120, 200)
+
         self.setWindowTitle(self.title)
         self.setGeometry(self.top, self.left, self.width, self.height)
         self.show()
-
-        #NEW CODE
-        
 
     def window2(self):                                             # <===
         self.w = Window2()
         self.w.show()
         #self.hide()
 
-    def getint(self):
-        print("yes")
+    #def print_LineEdit(self, text):
+        #line = lineEntry1.text()
+        #print(text)
+
+    def button_clicked(self):
+        value = self.lineEntry1.text()
+        print(value)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
